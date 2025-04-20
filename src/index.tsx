584f75d74ee4020dc6f9dbacac96b7367f419dd2
daviddeejjames <davidj28827@gmail.com>
2025-04-20 23:11:51 +1000

    Attempt fix solid router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import { ParentComponent } from 'solid-js';
+import { lazy } from 'solid-js';
+// import { ParentComponent } from 'solid-js';
 import { render } from 'solid-js/web';
-import { Router, Route } from '@solidjs/router';
+import { Router } from '@solidjs/router';
 import { MetaProvider } from '@solidjs/meta';
 
 import Home from './pages/Home';
@@ -8,18 +9,22 @@ import Blog from './pages/Blog';
 
 import './scss/styles.scss';
 
-const App: ParentComponent = ({ children }) => <>{children}</>;
-
-const root = document.getElementById('root') as unknown as HTMLElement;
+const routes = [
+  {
+    path: '/',
+    component: lazy(() => import('./pages/Home')),
+  },
+  {
+    path: '/blog',
+    component: lazy(() => import('./pages/Blog')),
+  },
+];
 
 render(
   () => (
     <MetaProvider>
-      <Router root={App}>
-        <Route path="/" component={Home} />
-        <Route path="/blog" component={Blog} />
-      </Router>
+      <Router>{routes}</Router>
     </MetaProvider>
   ),
-  root
+  document.getElementById('root')
 );
